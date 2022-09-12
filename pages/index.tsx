4d640b73617f95dfccadebb18cc7d57f90ff49cd
Eliel Nfinic <elielmathe@nfinic.com>
2022-09-13 00:08:24 +0200

    Adding log in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
 import Image from "next/image";
-import { useState } from "react";
+import React, { Component, useState } from "react";
 import BillsDashboard, { Bill } from "../components/BillsDashboard";
 import KYCDashboard from "../components/KYCDashboard";
 import SideBar from "../components/SideBar";
 
-export default function Home() {
-  const [currentTab, setCurrentTab] = useState<string>("bills");
-  const dummyData: Bill[] = [
+import { useRef,useEffect } from "react";
+import { render } from "sass";
+
+export default class Home extends Component{
+
+  state = {
+    tab : "bills"
+  }
+  
+  
+  //const [currentTab, setCurrentTab] = useState<string>("bills");
+   dummyData: Bill[] = [
     {
       amount: "100.000",
       client: "Actom Inc.",
@@ -40,41 +49,53 @@ export default function Home() {
       status: "Refused",
     },
   ];
-  return (
-    <div className="home-bg">
-      <div className="section-wrapper">
-        <div className="gap-16 flex flex-row py-10 justify-center">
-          <SideBar onSwitchTab={(tab) => setCurrentTab(tab)} />
-          <div className="flex-1">
-            <div className="topbar flex flex-row pb-5 justify-end items-center gap-10">
-              <div className="notif-container items-center flex justify-center">
-                <Image
-                  src="/assets/shared/notification.svg"
-                  height="28px"
-                  width="28px"
-                  alt="notifications"
-                />
+
+  componentDidMount(){
+    if(!localStorage.token){
+      window.location.href = "/login";
+    }
+    
+  }
+
+  render(){    
+    const currentTab = this.state.tab;
+    return (
+      <div className="home-bg">
+        <div className="section-wrapper">
+          <div className="gap-16 flex flex-row py-10 justify-center">
+            <SideBar onSwitchTab={(tab) => this.setState({tab:tab})} />
+            <div className="flex-1">
+              <div className="topbar flex flex-row pb-5 justify-end items-center gap-10">
+                <div className="notif-container items-center flex justify-center">
+                  <Image
+                    src="/assets/shared/notification.svg"
+                    height="28px"
+                    width="28px"
+                    alt="notifications"
+                  />
+                </div>
+                <div className="profile-wrapper flex flex-row px-6 py-5 rounded-3xl items-center gap-6">
+                  <h4>Eliel M.</h4>
+                  <Image
+                    src="/assets/shared/profile.svg"
+                    height="35px"
+                    width="35px"
+                    alt="profile"
+                  />
+                </div>
               </div>
-              <div className="profile-wrapper flex flex-row px-6 py-5 rounded-3xl items-center gap-6">
-                <h4>Eliel M.</h4>
-                <Image
-                  src="/assets/shared/profile.svg"
-                  height="35px"
-                  width="35px"
-                  alt="profile"
-                />
-              </div>
+              {currentTab === "bills" ? (
+                <BillsDashboard data={this.dummyData} />
+              ) : currentTab === "kyc" ? (
+                <KYCDashboard />
+              ) : (
+                <></>
+              )}
             </div>
-            {currentTab === "bills" ? (
-              <BillsDashboard data={dummyData} />
-            ) : currentTab === "kyc" ? (
-              <KYCDashboard />
-            ) : (
-              <></>
-            )}
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
 }
--- pages/login.tsx
+++ pages/login.tsx
@@ -3,73 +3,85 @@ import Button from "../components/shared/Button";
 import InputField from "../components/shared/InputField";
 import { serverInstance } from "../utils/apiServices";
 
-const Login = () => {
-  // STATES
-  const [lading, setLoading] = useState<boolean>(false);
-  const [form, setForm] = useState({
-    email: "",
-    password: "",
-  });
+import React,{Component} from "react";
 
-  const loginFields = [
-    { placeholder: "Email address" },
-    { placeholder: "Password" },
+export default class Login extends Component{
+  state = {
+    email : "",
+    password : "",
+    lading : ""
+  };
+
+  loginFields = [
+    { placeholder: "Email address", type : "email", name : "email" },
+    { placeholder: "Password", type: "password", name : "password" },
   ];
 
-  const handleLogin = async () => {
-    setLoading(true);
+  //component mounted
+  componentDidMount(){
+    if(localStorage.token){
+      window.location.href = "/";
+    }
+  }
+
+  handleLogin = async () => {
+    this.setState({loading : true});
     const data = await serverInstance.postRequest("login", {
-      email: "elimek2@gmail.com",
-      password: "pwd",
+      email: this.state.email,
+      password: this.state.password,
     });
     if (data) {
       localStorage.setItem("token", data.token);
       localStorage.setItem("user", JSON.stringify(data.user));
-      setLoading(false);
+      this.setState({loading : false});
+      window.location.href = "/";
       return;
     }
 
-    setLoading(false);
+    this.setState({loading : false});
   };
 
-  return (
-    <div className="section-wrapper login-page-wrapper gap-16 flex flex-row py-10 justify-center">
-      <div className="relative illustrations">
-        <h1 className="font-normal p-3 text-2xl leading-7 mb-5">SafiPay</h1>
-        <div className="login-illustration-bg" />
-        <div className="login-illustration-xs" />
-        <div className="circle-bg-xs absolute" />
-      </div>
-      <div className="flex-1">
-        <div className="flex flex-row justify-end">
-          <Button type="filled" width={130} link="/signup" label="Sign up" />
-        </div>
-        <div className="pr-24">
-          <div>
-            <p className="login-intro">Welcome back </p>
-          </div>
-          <div className="form">
-            <p className="intro-form">
-              Please enter your account details to log in
-            </p>
-            {loginFields.map((field, idx) => (
-              <InputField
-                key={idx}
-                placeholder={field.placeholder}
-                onTextChange={(text) => console.log(text)}
-              />
-            ))}
-            <div className="flex flex-row justify-end">
-              <p className="link-bottom my-6">
-                Did you forget your password? Reset it here
-              </p>
-            </div>
-            <Button type="filled" onClick={handleLogin} label="Log in" />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
 
-export default Login;
+  render(){
+    return (
+      <div className="section-wrapper login-page-wrapper gap-16 flex flex-row py-10 justify-center">
+        <div className="relative illustrations">
+          <h1 className="font-normal p-3 text-2xl leading-7 mb-5">SafiPay</h1>
+          <div className="login-illustration-bg" />
+          <div className="login-illustration-xs" />
+          <div className="circle-bg-xs absolute" />
+        </div>
+        <div className="flex-1">
+          <div className="flex flex-row justify-end">
+            <Button type="filled" width={130} link="/signup" label="Sign up" />
+          </div>
+          <div className="pr-24">
+            <div>
+              <p className="login-intro">Welcome back </p>
+            </div>
+            <div className="form">
+              <p className="intro-form">
+                Please enter your account details to log in
+              </p>
+              {this.loginFields.map((field, idx) => (
+                <InputField
+                  key={idx}
+                  type={field.type}
+                  placeholder={field.placeholder}
+                  onTextChange={(text) => this.setState({[field.name] : text})}
+                />
+              ))}
+              <div className="flex flex-row justify-end">
+                <p className="link-bottom my-6">
+                  Did you forget your password? Reset it here
+                </p>
+              </div>
+              <Button type="filled" onClick={this.handleLogin} label="Log in" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
