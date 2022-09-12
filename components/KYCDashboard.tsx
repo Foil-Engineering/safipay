f7540c29ec027f1295c5ea58166db9e1e063eec9
pacyL2K19 <pacilinja2@gmail.com>
2022-09-12 02:29:03 +0200

    feat: update the kyc page

---
+++ components/KYCDashboard.tsx
@@ -0,0 +1,11 @@
+const KYCDashboard = () => {
+  return (
+    <div className="kyc-tab-wrapper p-8">
+      <div className="header">
+        <h2>KYC verification</h2>
+      </div>
+    </div>
+  );
+};
+
+export default KYCDashboard;
